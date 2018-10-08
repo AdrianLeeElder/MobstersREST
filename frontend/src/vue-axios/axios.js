b3371c038fa757be5bfe59c859031a22570f58d1
Adrian Elder <AdrianLeeElder@gmail.com>
2018-10-08 02:26:46 +0100

    Fix hard coded heroku url

--- frontend/src/vue-axios/axios.js
+++ frontend/src/vue-axios/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const MOBSTERS_API_URL = process.env.MOBSTERS_API_URL || 'http://mobster-players-revenge.com/api/v1'
+const MOBSTERS_API_URL = process.env.MOBSTERS_API_URL || 'http://mobster-players-revenge.herokuapp.com/api/v1'
 
 var mobstersApi = axios.create({
   baseURL: MOBSTERS_API_URL,
