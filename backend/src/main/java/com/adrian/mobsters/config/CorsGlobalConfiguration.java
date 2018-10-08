c6a50d00d5285fa5a43b486ff7fff798b8a58252
Adrian Elder <AdrianLeeElder@gmail.com>
2018-10-08 02:04:25 +0100

    Use Heroku port environmental variable.

--- backend/src/main/java/com/adrian/mobsters/config/CorsGlobalConfiguration.java
+++ backend/src/main/java/com/adrian/mobsters/config/CorsGlobalConfiguration.java
@@ -10,7 +10,7 @@ public class CorsGlobalConfiguration implements WebFluxConfigurer {
 
     @Value("${CORS_ALLOWED_HOST:localhost}")
     private String allowedHost;
-    @Value("${CORS_ALLOWED_PORT:8081}")
+    @Value("${PORT:8081}")
     private int corsAllowedPort;
 
     @Override
