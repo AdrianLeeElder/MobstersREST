f4f65414bd19ad0a8b815537e12ebd0fac118c56
Adrian Elder <AdrianLeeElder@gmail.com>
2019-06-28 21:19:07 -0400

    Switch over to property based configuration instead of yml

--- src/main/java/com/adrian/mobsters/config/SwaggerConfig.java
+++ src/main/java/com/adrian/mobsters/config/SwaggerConfig.java
@@ -4,8 +4,11 @@ import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.*;
-import springfox.documentation.service.*;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -13,14 +16,12 @@ import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-    @Value("${otka.oauth2.client-id}")
+    @Value("${okta.oauth2.client-id}")
     private String clientId;
     @Value("${otka.oauth2.issuer}")
     private String authServer;
