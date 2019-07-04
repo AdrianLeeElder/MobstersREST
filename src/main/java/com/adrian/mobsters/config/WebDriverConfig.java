5313aa383609eca4b5cae150761b95ad6178db5b
Adrian Elder <AdrianLeeElder@gmail.com>
2019-07-04 10:51:12 -0400

    Set whitelisted ips so that docker can use chromedriver

--- src/main/java/com/adrian/mobsters/config/WebDriverConfig.java
+++ src/main/java/com/adrian/mobsters/config/WebDriverConfig.java
@@ -34,6 +34,7 @@ public class WebDriverConfig {
                 "--disable-notifications",
                 "--start-maximized",
                 "--no-sandbox",
+                "--whitelisted-ips",
                 "--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36",
                 "--lang=en");
         desiredCapabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
